80d13878f5ca8fc2ecbeea9f605f6bd60208ac1b
Vikram-Hegde <vikramhegde1440@gmail.com>
2024-01-08 20:35:45 +0530

    nav markup ✅

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,52 @@
+import ProfileImg from "@/assets/profile.png"
+import ChevronDownIcon from "@/assets/chevron-down.svg?react"
+import WalletIcon from "@/assets/wallet.svg?react"
+
+const navItems = [
+	"Home",
+	"Orders",
+	"Products",
+	"Delivery",
+	"Marketing",
+	"Analytics",
+	"Payments",
+	"Tools",
+	"Discounts",
+	"Audience",
+	"Appearance",
+	"Plugins",
+]
+
 const Sidebar = () => {
 	return (
 		<nav className="nav">
 			<div className="nav__profile">
-				<img src="" alt="" />
+				<img src={ProfileImg} alt="profile image" />
+				<div className="nav__profile-name">
+					<h3>Nishyan</h3>
+					<a href="#">Visit Store</a>
+					<ChevronDownIcon />
+				</div>
+			</div>
+			<ul className="nav__items">
+				{navItems.map((item, index) => (
+					<li className="nav__item" key={index}>
+						<img src={`/icons/NavIcon-${index}.svg`} />
+						<a href="#">{item}</a>
+					</li>
+				))}
+			</ul>
+			<div className="nav__credits">
+				<div className="icon-wrapper">
+					<WalletIcon />
+				</div>
+				<div className="nav__credit-points">
+					Available credits
+					<span>222.10</span>
+				</div>
 			</div>
 		</nav>
 	)
 }
 
-export default Sidebar;
+export default Sidebar
